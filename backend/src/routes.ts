726faedcca77830fed1a04198b6ca8d8529be305
IsabellaSoares <isabella.soares.28@gmail.com>
2020-10-17 12:24:38 -0300

    fix routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -7,8 +7,8 @@ import uploadConfig from './config/upload'
 const routes = Router()
 const upload = multer(uploadConfig)
 
-routes.get('orphanages', OrphanagesController.index)
-routes.get('orphanages/:id', OrphanagesController.show)
-routes.post('orphanages', upload.array('images'), OrphanagesController.create)
+routes.get('/orphanages', OrphanagesController.index)
+routes.get('/orphanages/:id', OrphanagesController.show)
+routes.post('/orphanages', upload.array('images'), OrphanagesController.create)
 
 export default routes
