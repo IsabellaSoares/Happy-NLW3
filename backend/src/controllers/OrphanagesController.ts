e3aa3649a0ccadbdda6b31171235efe2bd1835c1
IsabellaSoares <isabella.soares.28@gmail.com>
2020-10-17 00:16:51 -0300

    upload images

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -32,6 +32,9 @@ export default {
     
         const orphanagesRepository = getRepository(Orphanage)
     
+        const requestImages = request.files as Express.Multer.File[]
+        const images = requestImages.map(image => ({ path: image.filename }))
+
         const orphanage = orphanagesRepository.create({
             name,
             latitude,
@@ -39,7 +42,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         })
     
         await orphanagesRepository.save(orphanage)
