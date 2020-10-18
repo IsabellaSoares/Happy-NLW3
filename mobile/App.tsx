87e214896c5146ab3e157fc9464642babe18d0f0
IsabellaSoares <isabella.soares.28@gmail.com>
2020-10-18 00:38:09 -0300

    add google fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,10 +2,20 @@ import React from 'react';
 import { StyleSheet, View, Dimensions, Text, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
 import { Feather } from '@expo/vector-icons'
+import { useFonts } from 'expo-font'
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito'
 
 import MapMarker from './src/images/map-marker.png'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
       <MapView
@@ -58,6 +68,7 @@ const styles = StyleSheet.create({
     elevation: 3
   },
   calloutText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#0089a5',
     fontSize: 14
   },
@@ -79,6 +90,7 @@ const styles = StyleSheet.create({
     elevation: 3,
   },
   footerText: {
+    fontFamily: 'Nunito_700Bold',
     color: '#8fa7b3'
   },
   createOrphanageButton: {
