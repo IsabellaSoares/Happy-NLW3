6dc5b1673ebef22c0a83598f26ade30723e2381f
IsabellaSoares <isabella.soares.28@gmail.com>
2020-10-18 15:46:43 -0300

    fix map effect

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View, Dimensions, Text } from 'react-native'
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps'
 import { Feather } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useFocusEffect } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler'
 
 import api from '../services/api'
@@ -20,7 +20,7 @@ const OrphanagesMap = () => {
   const navigation = useNavigation()
   const [orphanages, setOrphanages] =useState<Orphanage[]>([])
   
-  useEffect(() => {
+  useFocusEffect(() => {
     api.get('orphanages').then(response => setOrphanages(response.data))
   }, [])
 
